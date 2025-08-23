4e0303e2d74e2efbbcc47f859b165a96783ce00b
KhanShaheb34 <shakirulhkhan@gmail.com>
2025-08-23 17:54:40 +0600

    Update package.json to include @vercel/analytics for enhanced analytics tracking and integrate Analytics component in layout.tsx for improved user insights.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from 'next';
 import { Fira_Mono } from 'next/font/google';
 import './globals.css';
+import { Analytics } from '@vercel/analytics/next';
 
 const firaMono = Fira_Mono({
   variable: '--font-fira-mono',
@@ -87,6 +88,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${firaMono.variable} antialiased`}>{children}</body>
+      <Analytics />
     </html>
   );
 }
